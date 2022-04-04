566d4d5e630d9b5e92bb60f327e701697480a227
ghazaly <medghazalyegy@gmail.com>
2022-04-04 13:41:44 +0000

    Upload Files

---
+++ 1. Requirements.py
@@ -0,0 +1,26 @@
+# Solution of Challenge 
+
+'''
+Functional
+
+    The system must.....
+
+        Maintain a library of albums and songs
+        Allow users to browse albums and songs
+        Allow users to select individual songs
+        Prevent users from selecting entire albums
+        Maintain a queue of songs to play
+        Play music
+        Allow users to sort by artist
+        Identify individual users
+        Track number of plays per user
+
+Non-Functional
+
+    The system should be......
+
+        Intuitive to use while floating in space
+        Available 24/7
+        Low-power
+        Updatable
+'''
---
+++ 2. Use Cases and use Stories.py
@@ -0,0 +1,33 @@
+# Solution of the challange
+
+'''
+Fist  Use Case
+
+    Tilte: Play Song 
+    Primary Actor: User
+    Success Scenario: System identifies user. User browses
+    library of available albums. User selects an album and
+    browses list of songs on the selected album. User selects 
+    a song. System plays the selected song.
+
+Second Use Case
+    
+    Title: Select Multiple Songs
+    Primary Actor: User
+    Succedd Scenario:
+        1 System identifies user.
+        2 User browses available albums and songs.
+        3 User selects a song.
+        4 System begins playing selected song.
+        5 User continues browsing and selects a second song.
+        6 System adds second song to play queue.
+        7 System plays second song after first song is over. 
+    User Stories:
+        As a user, i want my song to be added to the front of a 
+        long play queue, so that i don't have to wait hours to heat it.
+        As a user, i want to be identified without touching anything,
+        so that my hands are free to do other things.
+        As a user, i want to sort and browses songs by artist,
+        so that I can listen to every song by MAHER ZEIN.
+
+'''
